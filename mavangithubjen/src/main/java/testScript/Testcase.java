c4bdd4532e9c6915927e9dc5101b9c5acfea29ed
WDSI <WDSI@DESKTOP-BVO59HO>
2020-09-15 19:53:28 +0530

    Test commit by prasanth

---
+++ mavangithubjen/src/main/java/testScript/Testcase.java
@@ -0,0 +1,15 @@
+package testScript;
+
+import org.testng.annotations.Test;
+
+public class Testcase {
+	
+	
+	@Test
+	public void Run() {
+		
+		System.out.println("Am chitti version 3.0......");
+		
+	}
+
+}
